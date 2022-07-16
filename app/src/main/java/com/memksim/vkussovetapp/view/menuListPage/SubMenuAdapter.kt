c92a33c8096b524b240bc7a1817d9ff4a0e0c77d
memksim <memksim@vk.com>
2022-07-16 11:13:45 +0500

    Добавил прослушиватель нажатий на кнопку подменю

--- app/src/main/java/com/memksim/vkussovetapp/view/menuListPage/SubMenuAdapter.kt
+++ app/src/main/java/com/memksim/vkussovetapp/view/menuListPage/SubMenuAdapter.kt
@@ -1,9 +1,12 @@
 package com.memksim.vkussovetapp.view.menuListPage
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.memksim.vkussovetapp.R
 import com.memksim.vkussovetapp.databinding.SubmenuItemBinding
 import com.memksim.vkussovetapp.model.SubMenu
 import com.squareup.picasso.Picasso
@@ -12,7 +15,7 @@ class SubMenuAdapter(
 ): RecyclerView.Adapter<SubMenuAdapter.SubMenuViewHolder>() {
     var submenuList: List<SubMenu> = emptyList()
 
-    inner class SubMenuViewHolder(val binding: SubmenuItemBinding): RecyclerView.ViewHolder(binding.root){
+    inner class SubMenuViewHolder(val binding: SubmenuItemBinding,private val context: Context): RecyclerView.ViewHolder(binding.root){
         fun onBind(position: Int){
             Picasso.get().load("https://vkus-sovet.ru/${submenuList[position].image}").into(binding.image)
             binding.nameTitle.text = submenuList[position].name
@@ -25,17 +28,37 @@ class SubMenuAdapter(
                 binding.spicyImg.visibility = View.GONE
             }
         }
+
+        fun buttonClicked(){
+            if(binding.addToCardButton.text.equals(context.resources.getString(R.string.toCart))){
+                binding.addToCardButton.apply {
+                    setBackgroundColor(ResourcesCompat.getColor(context.resources, R.color.secondary, null))
+                    text = context.resources.getString(R.string.inCart)
+                }
+            }else{
+                binding.addToCardButton.apply {
+                    setBackgroundColor(ResourcesCompat.getColor(context.resources, R.color.accent, null))
+                    text = context.resources.getString(R.string.toCart)
+                }
+            }
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubMenuViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = SubmenuItemBinding.inflate(inflater)
 
-        return SubMenuViewHolder(binding)
+        return SubMenuViewHolder(binding, parent.context)
     }
 
     override fun onBindViewHolder(holder: SubMenuViewHolder, position: Int) {
         holder.onBind(position)
+
+        holder.binding.addToCardButton.setOnClickListener {
+            holder.buttonClicked()
+        }
+
     }
 
     override fun getItemCount(): Int = submenuList.size
